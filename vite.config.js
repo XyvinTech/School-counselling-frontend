fdb936c930dcbd608db0a83203806d84a18dfecc
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2024-07-29 22:17:49 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react-swc';
+import svgr from '@svgr/rollup';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(),],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
   },
 });
